9796b0d2b72946a884f170dbf07137105b85c7a5
gusworkout <tavoaod@gmail.com>
2024-07-28 15:51:25 -0500

    c1

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-let cantidad_producto = 0;
-
-let numeroDeProductos = document.getElementById("cantidadProductos");
 
 
-function AgregarAlCarrito(){
-    cantidad_producto ++;
-    numeroDeProductos.textContent = cantidad_producto;
-}
+
+    
 
 
-let producto = [ ];
+
+
 
 document.addEventListener('DOMContentLoaded', function(){
+    //DATOS NUEVOS
+     let cantidad_producto = 0;
+     let producto = [ ];
+     let numeroDeProductos = document.getElementById("cantidadProductos");
 
     //OBTENER LOS DATOS ALMACENADOS EN LOCAL STORAGE
     if(localStorage.getItem('producto')){
@@ -22,10 +22,10 @@ document.addEventListener('DOMContentLoaded', function(){
     document.querySelectorAll('.agregar_carrito').forEach(button => {
         button.addEventListener('click', function(){
 
-            const productos = this.closest('.producto')
+            const productos = this.closest('.producto1');
              
-            const nombre = producto.querySelector('.nombre_producto').innerText
-            const precio = producto.querySelector('.Precio_producto').innerText
+            const nombre = productos.querySelector('.nombre_producto').innerText
+            const precio = productos.querySelector('.Precio_producto').innerText
             
 
             const data = {nombre, precio};
@@ -35,9 +35,14 @@ document.addEventListener('DOMContentLoaded', function(){
             localStorage.setItem('productos', JSON.stringify(producto))
 
             alert('Se agregó con exito el ${nombre} al carrito')
+            cantidad_producto ++;
+            numeroDeProductos.textContent = cantidad_producto;
         })
         
     });
+    
+    
+
 
 })
 
