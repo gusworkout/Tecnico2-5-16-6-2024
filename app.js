c7891be106907929bd6e33ad103bedfbd124e730
gusworkout <tavoaod@gmail.com>
2024-07-28 15:31:12 -0500

    c7

--- app.js
+++ app.js
@@ -11,5 +11,33 @@ function AgregarAlCarrito(){
 
 let producto = [ ];
 
+document.addEventListener('DOMContentLoaded', function(){
 
+    //OBTENER LOS DATOS ALMACENADOS EN LOCAL STORAGE
+    if(localStorage.getItem('producto')){
+        producto = JSON.parse(localStorage.getItem('producto'))
+    }
+
+    //METODO PARA EL BOTON AGREGAR
+    document.querySelectorAll('.agregar_carrito').forEach(button => {
+        button.addEventListener('click', function(){
+
+            const productos = this.closest('.producto')
+             
+            const nombre = producto.querySelector('.nombre_producto').innerText
+            const precio = producto.querySelector('.Precio_producto').innerText
+            
+
+            const data = {nombre, precio};
+
+            productos.push(data);
+
+            localStorage.setItem('productos', JSON.stringify(producto))
+
+            alert('Se agregó con exito el ${nombre} al carrito')
+        })
+        
+    });
+
+})
 
